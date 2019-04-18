0a14fa8591ca5741f3e7dfe517ad80e68b429e03
Tesfachin <tesfachingizaw@gmail.com>
2019-04-18 14:50:37 -0400

    addition source files

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// Global app controller
+
+// import num from './test';
+// const x=23;
+// console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+
+import str from './models/Search';
+
+import {add as a, multiply as m, ID} from './views/searchView';
+
+console.log(`Using imported functions! ${a(ID,2)} and ${m(3,5)}.${str}`);
